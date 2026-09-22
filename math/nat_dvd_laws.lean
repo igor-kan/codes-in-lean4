62c9c01135648ead9679a5b7fdbe810b16fb5744
igor-kan <igorkan010@gmail.com>
2026-09-22 13:13:09 -0400

    feat: implement nat_dvd_laws.lean

---
+++ math/nat_dvd_laws.lean
@@ -0,0 +1,10 @@
+namespace NatDvdLaws
+
+theorem dvd_refl'' (n : Nat) : n ∣ n := Nat.dvd_refl n
+theorem dvd_trans'' {a b c : Nat} (h1 : a ∣ b) (h2 : b ∣ c) : a ∣ c := Nat.dvd_trans h1 h2
+theorem dvd_zero'' (n : Nat) : n ∣ 0 := Nat.dvd_zero n
+theorem one_dvd'' (n : Nat) : 1 ∣ n := Nat.one_dvd n
+theorem dvd_mul_right'' (a b : Nat) : a ∣ a * b := Nat.dvd_mul_right a b
+theorem dvd_mul_left'' (a b : Nat) : a ∣ b * a := Nat.dvd_mul_left a b
+
+end NatDvdLaws
