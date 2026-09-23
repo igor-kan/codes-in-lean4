65be9f63962bba391d0189eb26c090d38cdd39db
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:28 -0400

    feat: implement insertion_47.lean

---
+++ sorting/insertion_47.lean
@@ -0,0 +1,11 @@
+namespace Geninsertion_47
+
+def insert (x : Nat) : List Nat → List Nat
+  | [] => [x]
+  | y :: ys => if x ≤ y then x :: y :: ys else y :: insert x ys
+def insSort : List Nat → List Nat
+  | [] => []
+  | x :: xs => insert x (insSort xs)
+theorem st : insSort [83, 95, 88] = [83, 88, 95] := by decide
+
+end Geninsertion_47
